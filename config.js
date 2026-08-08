e04421dad59d2328630f11076f3080e76c3dc2ba
mishlef <mishlef01@gmail.com>
2026-08-08 14:27:13 -0400

    Set WEBHOOK_URL to the deployed Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste the Web App URL you get from deploying the Apps Script (see README.md).
 // Leave as an empty string to disable submission (CSV export will still work).
-const WEBHOOK_URL = "";
+const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxBZcuY7GIB3Qw5uycmUjPV5mH-gnKO9r712u14S4IPAkh0hGIBpnRhAqnpkmDen69FOw/exec";
